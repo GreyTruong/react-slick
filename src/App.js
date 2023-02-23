d8cceb247f11a0070268dc3d2248a6f17e70a93d
Quan Truong <trquan306@gmail.com>
2023-02-23 10:48:49 +0700

    rename files.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React, { useState } from "react";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Top4 from "./cLink";
-import TopBigBanner from "./cSliders";
+import Top4 from "./Top4";
+import TopBigBanner from "./TopBigBanner";
 function App() {
   const [slideIndex, setSlideIndex] = useState(0);
   return (
